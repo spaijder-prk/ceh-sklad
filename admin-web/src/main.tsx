7331c6f6f943600657aac437c0b030b1e05245cb
spaijder-prk <anatolijkrllv@gmail.com>
2026-09-04 07:06:29 +0700

    Панель состояния системы подключена к web-интерфейсу

--- admin-web/src/main.tsx
+++ admin-web/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import AuditPanel from './AuditPanel'
 import ManagementExtensions from './ManagementExtensions'
+import SystemStatusPanel from './SystemStatusPanel'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SystemStatusPanel />
     <ManagementExtensions />
     <AuditPanel />
   </React.StrictMode>,
